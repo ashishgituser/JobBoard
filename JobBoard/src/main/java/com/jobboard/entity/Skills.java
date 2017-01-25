8a9564a0aae18a635fa03abfcc922dd58624db8e
ashishch <ashishch@192.168.1.4>
2017-01-25 22:41:07 +0530

    entity

--- JobBoard/src/main/java/com/jobboard/entity/Skills.java
+++
@@ -1,6 +0,0 @@
-package com.jobboard.entity;
-
-public class Skills {
-	private int id;
-	private String name;
-}
--- JobBoard/src/main/java/com/jobboard/security/SecurityWebAppInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.jobboard.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
